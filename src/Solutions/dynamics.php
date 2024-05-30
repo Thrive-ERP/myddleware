dfd5af66adf8c83bd6322f49845954b48264c6a6
cpichaud <camillepichaud44@gmail.com>
2024-05-30 12:08:42 +0200

    get token

--- src/Solutions/dynamics.php
+++ src/Solutions/dynamics.php
@@ -25,15 +25,11 @@
 namespace App\Solutions;
 
 use Microsoft\Graph\GraphServiceClient;
-use Microsoft\Kiota\Abstractions\Store\BackedModel;
-use Microsoft\Kiota\Abstractions\RequestInformation;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
 use Microsoft\Graph\Core\Authentication\GraphPhpLeagueAccessTokenProvider;
-use Microsoft\Kiota\Abstractions\Authentication\BaseBearerTokenAuthenticationProvider;
-use YourNamespace\YourAccessTokenProvider;
 
 class DynamicsCore extends Solution
 {
@@ -41,8 +37,9 @@ class DynamicsCore extends Solution
     public static $tenantId;
     public static $clientId;
     public static $clientSecret;
-
+    public static $tokenContext;
     protected $last_error = '';
+
 
     public function getFieldsLogin(): array
     {
@@ -70,39 +67,32 @@ class DynamicsCore extends Solution
         self::$clientId = $paramConnexion['clientid'];
         self::$clientSecret = $paramConnexion['clientsecret'];
         self::$tenantId = $paramConnexion['tenantid'];
-
+    
         $scopes = ['https://graph.microsoft.com/.default'];
-
-        $tokenContext = new ClientCredentialContext(
+    
+        self::$tokenContext = new ClientCredentialContext(
             self::$tenantId,
             self::$clientId,
             self::$clientSecret
         );
-
-        try {
-            $this->graphClient = new GraphServiceClient($tokenContext, $scopes);
-            $accessToken = $this->getAppOnlyToken();
-            dump("Connecté avec succès. Token: " . $accessToken);
-            die();
-            // $authenticationProvider = new ClientCredentialProvider($tokenContext);
-            // $this->graphClient = GraphServiceClient::createWithAuthenticationProvider($authenticationProvider);
-            // $accessToken = $this->getAppOnlyToken();
-            // echo "Connecté avec succès. Token: " . $accessToken . "\n";
     
-            return true;
+        try {
+            $this->graphClient = new GraphServiceClient(self::$tokenContext, ['https://graph.microsoft.com/.default']);
+            $accessToken = $this->getAppOnlyToken();
+            $this->connexion_valide = true;
         } catch (Exception $e) {
-            $this->last_error = "Erreur lors de la connexion : " . $e->getMessage();
-            return false;
+            $error = $e->getMessage();
+            $this->logger->error($error);
+            return ['error' => $error];
         }
     }
 
-    private function getAppOnlyToken()
-{
-    //$tokenProvider = new GraphPhpLeagueAccessTokenProvider($this->graphClient->createWithAuthenticationProvider());
-    //return $this->graphClient->getAuthenticationProvider()->getAuthorizationTokenAsync('https://graph.microsoft.com/.default').wait();
-    //return $tokenProvider->getAuthorizationTokenAsync('https://graph.microsoft.com/.default')->wait();
-    return $this->graphClient->getAuthenticationProvider()->getAccessToken();
-}
+    private function getAppOnlyToken() {
+        $tokenProvider = new GraphPhpLeagueAccessTokenProvider(self::$tokenContext);
+        return $tokenProvider
+            ->getAuthorizationTokenAsync('https://graph.microsoft.com/.default')
+            ->wait();
+    }
 }
 
 class dynamics extends dynamicscore
